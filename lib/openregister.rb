af3fac2466b5fc2456b7ddaf1c2f63bd8205dd2b
robmckinnon <rob@movingflow>
2016-03-05 15:00:14 +0000

    Convert each results page to objects as retrieved
    Instead of appending ressult page bodies to a list of results
    before converting to objects.

--- lib/openregister.rb
+++ lib/openregister.rb
@@ -69,14 +69,15 @@ module OpenRegister
 
     def retrieve url, type, from_openregister, all=false, page_size=100
       list = augment_register_fields(from_openregister) do
-        url = url+'.tsv'
+        url = "#{url}.tsv"
         url = "#{url}?page-index=1&page-size=#{page_size}" if page_size != 100
-        response_list = response_list(url, all)
-        if response_list.first[/^entry/]
-          response_list.map { |tsv| Morph.from_tsv(tsv, type, OpenRegister) }.flatten
-        else
-          response_list.map { |json| Morph.from_json(json, type, OpenRegister) }.flatten
+        results = []
+        response_list(url, all) do |tsv|
+          items = Morph.from_tsv(tsv, type, OpenRegister)
+          items.each {|item| results.push item }
+          nil
         end
+        results
       end
       list.each { |item| item.from_openregister = true } if from_openregister
       list
@@ -90,24 +91,19 @@ module OpenRegister
       end
     end
 
-    def response_list url, all
+    def response_list url, all, &block
       response = RestClient.get(url)
-      if response.body[/^entry/]
-        tsv = response.body
-        list = [tsv]
-      else
-        json = munge response.body
-        list = [json]
-      end
+      tsv = response.body
+      yield tsv
       if all && link_header = response.headers[:link]
         if rel_next = links(link_header)[:next]
           next_url = "#{url.split('?').first}#{rel_next}"
-          list << response_list(next_url, all)
+          response_list(next_url, all, &block)
         end
       end
-      list.flatten
+      nil
     rescue RestClient::ResourceNotFound => e
-      []
+      puts e.to_s
     end
 
     def munge json
